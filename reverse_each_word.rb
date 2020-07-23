a58d624e63c22b975559a3aaa4113a4f0c0fe8a8
Luis Garcia <l.garcia24@hotmail.com>
2020-07-23 14:03:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
-require 'pry'
 def reverse_each_word(string)
-  #binding.pry
-  #string_arr = []
-  
-  #string_arr << string.split(' ')
   
   (string.split).collect do |element|
    element.reverse
